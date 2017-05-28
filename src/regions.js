f4cfee615733f166a9d2e3ebde8e6754307e540c
Michał Kownacki <michal_kownacki@o2.pl>
2017-05-28 22:09:24 +0200

    Add `regions.js`.

---
+++ src/regions.js
@@ -0,0 +1,137 @@
+imports.regions = {
+  crag1: {
+    actions: [{
+      type: 'roll',
+      results: {
+        1: [{
+          type: 'addLifes',
+          value: -1,
+        }],
+        2: [{
+          type: 'addLifes',
+          value: -1,
+        }],
+      },
+      includeFriends: true,
+    }],
+    left: 'strongholdRuins',
+    right: 'wolvenRavine',
+  },
+  strongholdRuins: {
+    actions: [{
+      type: 'cards',
+      count: 1,
+    }],
+    left: 'tolimanTemple',
+    right: 'crag1',
+  },
+  tolimanTemple: {
+    actions: [{
+      type: 'doubleRoll',
+      results: {
+        2: [{
+          type: 'addLifes',
+          value: -1,
+        }],
+        3: [{
+          type: 'addMagic',
+          value: -1,
+        }, {
+          type: 'addSword',
+          value: -1,
+        }],
+        4: [{
+          type: 'loseSpell',
+        }],
+        5: [{
+          type: 'loseFriend',
+        }],
+        6: [{
+          type: 'gainSpell',
+        }],
+        7: [{
+          type: 'choose',
+          options: [{
+            type: 'addMagic',
+            value: 1,
+          }, {
+            type: 'addSword',
+            value: 1,
+          }]
+        }],
+        8: [{
+          type: 'gainMove',
+        }],
+        9: [{
+          type: 'curse',
+        }],
+        10: [{
+          type: 'gainItem',
+          item: 'tolimanShield',
+        }],
+        11:  [{
+          type: 'addLifes',
+          value: 1,
+        }],
+        12:  [{
+          type: 'addLifes',
+          value: -1,
+        }, {
+          type: 'addMagic',
+          value: -1,
+        }, {
+          type: 'addSword',
+          value: -1,
+        }],
+      },
+      includeFriends: true,
+    }],
+    left: 'skullValley',
+    right: 'strongholdRuins',
+  },
+  skullValley: {
+    left: 'swamps1',
+  },
+  swamps1: {
+    left: 'movingRocks1',
+  },
+  movingRocks1: {
+    left: 'crag2',
+  },
+  crag2: {
+    left: 'grassPlain',
+  },
+  grassPlain: {
+    left: 'crossroads1',
+  },
+  crossroads1: {
+    left: 'castle',
+  },
+  castle: {
+    left: 'ghostTown',
+  },
+  ghostTown: {
+    left: 'movingRocks2',
+  },
+  movingRocks2: {
+    left: 'swamps2',
+  },
+  swamps2: {
+    left: 'specterCrypt',
+  },
+  specterCrypt: {
+    left: 'sleepPlain',
+  },
+  sleepPlain: {
+    left: 'crossroads2',
+  },
+  crossroads2: {
+    left: 'stoneForest',
+  },
+  stoneForest: {
+    left: 'wolvenRavine',
+  },
+  wolvenRavine: {
+    left: 'crag1',
+  },
+};
