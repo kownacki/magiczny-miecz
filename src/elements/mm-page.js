fb7ba32d636e53a242a24901299e6795c36464f7
Michał Kownacki <mk@michalkownacki.pl>
2020-06-24 19:28:57 +0200

    Move fields to separate file.

--- src/elements/mm-page.js
+++ src/elements/mm-page.js
@@ -2,36 +2,7 @@ import {LitElement, html, css} from 'lit-element';
 import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
 import dbSyncMixin from 'mkwc/dbSyncMixin.js';
 import fb from '../utils/firebase.js';
-
-const fields = {
-  1: {adjacent: [2, 14], name: 'Karczma', description: `MUSISZ RZUCIĆ KOSTKĄ:
-1 - przegrałeś w kości 1 Sz. Z;
-2 - wygrałeś 1 Sz. Z;
-3 - musisz tu nocować, tracisz 1 turę;
-4 - musisz stawić czoła miejscowemu osiłkowi (Miecz 4);
-5 - poczęstowano cię eliksirem, dzięki któremu możesz przenieść się do dowolnego miejsca w tym Kręgu;
-6 - eliksir, który ci podano może przenieść cię do Świątyni Nemed.`},
-  2: {adjacent: [3, 1], name: 'Mroźne Pustkowie', description: `WYCIĄGNIJ 1 KARTĘ.
-Nie ciągnij Karty jeśli jakaś już tu jest.`},
-  3: {adjacent: [4, 2], name: 'Gród', description: `MOŻESZ TU ODWIEDZIĆ:
-<strong>Wróżbitę</strong>: rzuć kostką
-1 - zyskujesz 1 Zaklęcie;
-2 - zostajesz Zaklęty w Kamień;
-3 - jeżeli jesteś Zły stajesz się Dobry. Jeżeli jesteś Chaotyczny stajesz się Zły;
-4-6 zostałeś zignorowany.
-<strong>Lichwiarza</strong> - możesz wymienić dowolne Przedmioty na złoto (odłóż ich Karty i weź po 1 Sz. Z za każdy).`},
-  4: {adjacent: [5, 3], name: 'Bezdroża'},
-  5: {adjacent: [6, 4], name: 'Studnia Wieczności'},
-  6: {adjacent: [7, 5], name: 'Krąg Mocy'},
-  7: {adjacent: [8, 6], name: 'Czarci Młyn'},
-  8: {adjacent: [9, 7], name: 'Mokradła'},
-  9: {adjacent: [10, 8], name: 'Step'},
-  10: {adjacent: [11, 9], name: 'Osada'},
-  11: {adjacent: [12, 10], name: 'Kurhan'},
-  12: {adjacent: [13, 11], name: 'Mokradła'},
-  13: {adjacent: [14, 12], name: 'Step'},
-  14: {adjacent: [1, 13], name: 'Uroczysko'},
-};
+import fields from '../fields.js';
 
 export default class MmPage extends dbSyncMixin('_page', LitElement) {
   static get properties() {
